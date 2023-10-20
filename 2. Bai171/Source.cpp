1e6afa46c5c67d7eedffec2e7338ecaedbe50a41
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-10-20 20:53:42 +0700

    🚑 (2. Bai171): Algorithm changed

--- 2. Bai171/Source.cpp
+++ 2. Bai171/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 struct DaThuc
@@ -22,28 +23,39 @@ DATHUC operator/(DATHUC, DATHUC);
 int main()
 {
 	DATHUC f;
+	cout << "Nhap f:" << endl;
 	Nhap(f);
+
 	DATHUC g;
+	cout << "Nhap g:" << endl;
 	Nhap(g);
+
+	cout << "Thuong cua f / g:" << endl;
 	Xuat(f / g);
 	return 0;
 }
 
 void Nhap(DATHUC &f)
 {
-	cout << "Nhap n:";
+	cout << "Nhap bac da thuc: ";
 	cin >> f.n;
 	for (int i = f.n; i >= 0; i--)
 	{
-		cout << "Nhap a" << i << ":";
+		cout << "Nhap he so a[" << i << "]: ";
 		cin >> f.a[i];
 	}
 }
 
 void Xuat(DATHUC f)
 {
-	for (int i = f.n; i >= 0; i--)
-		cout << f.a[i] << "^" << i << "+";
+	cout << setw(6);
+	cout << setprecision(3);
+	for (int i = f.n; i >= 1; i--)
+	{
+		cout << f.a[i];
+		cout << "x^" << i;
+	}
+	cout << f.a[0];
 }
 
 int LonNhat(int a, int b)
@@ -91,23 +103,44 @@ DATHUC operator*(DATHUC f, DATHUC g)
 			temp.a[i + j] += f.a[i] * g.a[j];
 	return temp;
 }
+
+// DATHUC operator/(DATHUC f, DATHUC g)
+// {
+// 	DATHUC bichia = f;
+// 	DATHUC chia = g;
+// 	DATHUC temp;
+// 	temp.n = f.n - g.n;
+// 	for (int i = temp.n; i >= 0; i--)
+// 		temp.a[i] = 0;
+// 	while (bichia.n >= chia.n)
+// 	{
+// 		DATHUC tg;
+// 		tg.n = bichia.n - chia.n;
+// 		for (int i = tg.n; i >= 0; i--)
+// 			tg.a[i] = 0;
+// 		tg.a[tg.n] = bichia.a[bichia.n] / chia.a[chia.n];
+// 		DATHUC tru = tg * bichia;
+// 		temp = temp + tg;
+// 	}
+// 	return temp;
+// }
+
 DATHUC operator/(DATHUC f, DATHUC g)
 {
-	DATHUC bichia = f;
-	DATHUC chia = g;
-	DATHUC temp;
-	temp.n = f.n - g.n;
-	for (int i = temp.n; i >= 0; i--)
-		temp.a[i] = 0;
-	while (bichia.n >= chia.n)
+	DATHUC thuong;
+	thuong.n = f.n - g.n;
+	for (int i = thuong.n; i >= 0; i--)
+		thuong.a[i] = 0;
+	for (int i = thuong.n; i >= 0; i--)
 	{
-		DATHUC tg;
-		tg.n = bichia.n - chia.n;
-		for (int i = tg.n; i >= 0; i--)
-			tg.a[i] = 0;
-		tg.a[tg.n] = bichia.a[bichia.n] / chia.a[chia.n];
-		DATHUC tru = tg * bichia;
-		temp = temp + tg;
+		thuong.a[i] = f.a[i + g.n] / g.a[g.n];
+		DATHUC temp;
+		temp.n = i + g.n;
+		for (int i = temp.n; i >= 0; i--)
+			temp.a[i] = 0;
+		for (int j = temp.n; j >= i; j--)
+			temp.a[j] = thuong.a[i] * g.a[temp.n - i];
+		f = f - temp;
 	}
-	return temp;
+	return thuong;
 }
