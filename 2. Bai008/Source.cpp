c8cf1901a397807ecfad36fbe9969c2e36ede963
MinhHuy1507 <ngominhhuy150705@gmail.com>
2023-10-21 23:04:03 +0700

    2.003 - 2.028 Done

--- 2. Bai008/Source.cpp
+++ 2. Bai008/Source.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 using namespace std;
 
+struct DonThuc
+{
+	float a;
+	int n;
+};
+typedef struct DonThuc DONTHUC;
+
+void Nhap(DONTHUC&);
+void Xuat(DONTHUC);
+
 int main()
 {
-
+	DONTHUC f;
+	Nhap(f);
+	cout << "\nDon thuc: ";
+	Xuat(f);
 	return 0;
+}
+
+void Nhap(DONTHUC& f)
+{
+	cout << "Nhap he so: ";
+	cin >> f.a;
+	cout << "Nhap so mu: ";
+	cin >> f.n;
+}
+
+void Xuat(DONTHUC f)
+{
+	cout << "\nHe so: " << f.a;
+	cout << "\nSo mu: " << f.n;
 }
--- 2. Bai028/Source.cpp
+++ 2. Bai028/Source.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
 using namespace std;
 
+struct diem
+{
+	float x;
+	float y;
+};
+typedef struct diem DIEM;
+
+void Nhap(DIEM&);
+void Xuat(DIEM);
+bool KtThuoc2(DIEM);
+
 int main()
 {
+	DIEM A;
+	Nhap(A);
+	Xuat(A);
 
+	if (KtThuoc2(A))
+		cout << "\nDiem thuoc phan tu thu 2";
+	else
+		cout << "\nDiem khong thuoc phan tu thu 2";
 	return 0;
+}
+
+void Nhap(DIEM& P)
+{
+	cout << "Nhap x: ";
+	cin >> P.x;
+	cout << "Nhap y: ";
+	cin >> P.y;
+}
+
+void Xuat(DIEM P)
+{
+	cout << "(" << P.x << ", " << P.y << ")";
+}
+
+bool KtThuoc2(DIEM P)
+{
+	if (P.x < 0 && P.y>0)
+		return true;
+	return false;
 }
