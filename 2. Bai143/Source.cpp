64e20e49f1f2da8298ae173bde9f360f029b5515
MinhHuy1507 <ngominhhuy150705@gmail.com>
2023-10-23 13:05:24 +0700

    2.103-2.153 Done

--- 2. Bai143/Source.cpp
+++ 2. Bai143/Source.cpp
@@ -1,8 +1,66 @@
 #include <iostream>
 using namespace std;
 
+struct Diem
+{
+	float x;
+	float y;
+};
+typedef struct Diem DIEM;
+
+struct TamGiac
+{
+	DIEM A;
+	DIEM B;
+	DIEM C;
+};
+typedef struct TamGiac TAMGIAC;
+
+void Nhap(DIEM&);
+void Nhap(TAMGIAC&);
+void Xuat(DIEM);
+DIEM TungNhoNhat(TAMGIAC t);
+
 int main()
 {
+	TAMGIAC t;
+	Nhap(t);
 
+	cout << "\nDiem co tung do nho nhat la: ";
+	Xuat(TungNhoNhat(t));
 	return 0;
 }
+
+void Nhap(DIEM& I)
+{
+	cout << "\nNhap x: ";
+	cin >> I.x;
+	cout << "Nhap y: ";
+	cin >> I.y;
+}
+
+void Nhap(TAMGIAC& t)
+{
+	cout << "\nNhap A: ";
+	Nhap(t.A);
+	cout << "\nNhap B: ";
+	Nhap(t.B);
+	cout << "\nNhap C: ";
+	Nhap(t.C);
+}
+
+void Xuat(DIEM I)
+{
+	cout << "(" << I.x << ", " << I.y << ")";
+}
+
+DIEM TungNhoNhat(TAMGIAC t)
+{
+	DIEM lc = t.A;
+	if (t.B.y < lc.y)
+		lc = t.B;
+	if (t.C.y < lc.y)
+		lc = t.C;
+	return lc;
+}
+
--- 2. Bai148/Source.cpp
+++ 2. Bai148/Source.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 using namespace std;
 
+struct DuongThang
+{
+	float a;
+	float b;
+	float c;
+};
+typedef struct DuongThang DUONGTHANG;
+
+void Nhap(DUONGTHANG&);
+void Xuat(DUONGTHANG);
+
 int main()
 {
+	DUONGTHANG d;
+	Nhap(d);
+	Xuat(d);
 
 	return 0;
+}
+
+void Nhap(DUONGTHANG& d)
+{
+	cout << "\nNhap a: ";
+	cin >> d.a;
+	cout << "Nhap b: ";
+	cin >> d.b;
+	cout << "Nhap c: ";
+	cin >> d.c;
+}
+
+void Xuat(DUONGTHANG d)
+{
+	cout << "\na: " << d.a;
+	cout << "\nb: " << d.b;
+	cout << "\nc: " << d.c;
 }
