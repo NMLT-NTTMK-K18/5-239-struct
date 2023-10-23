e360e3ef6c24b696439b9878a37295e5eb54ccf1
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-23 22:05:46 +0700

    Bai015. Fixed + Bai020. Completed

--- 2. Bai015/Source.cpp
+++ 2. Bai015/Source.cpp
@@ -1,4 +1,24 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+struct Diem
+{
+	float x;
+	float y;
+};
+typedef struct Diem DIEM;
+
 void Nhap(DIEM&);
+void Xuat(DIEM);
+
+int main()
+{
+	DIEM P;
+	Nhap(P);
+	Xuat(P);
+	return 0;
+}
 
 void Nhap(DIEM& P)
 {
@@ -6,4 +26,12 @@ void Nhap(DIEM& P)
 	cin >> P.x;
 	cout << "Nhap y: ";
 	cin >> P.y;
+}
+
+void Xuat(DIEM P)
+{
+	cout << setw(10);
+	cout << setprecision(2);
+	cout << "\nx: " << P.x;
+	cout << "\ny: " << P.y;
 }
