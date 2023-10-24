83383de037194041a986945f8abfbb69b4b8f557
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-24 19:53:57 +0700

    Bai095. Completed

--- 2. Bai095/Source.cpp
+++ 2. Bai095/Source.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
 using namespace std;
 
+struct Ngay
+{
+	int Ngay;
+	int Thang;
+	int Nam;
+};
+typedef struct Ngay NGAY;
+
+void Nhap(NGAY&);
+int ktNhuan(NGAY);
+int SoNgayToiDaTrongThang(NGAY);
+
 int main()
 {
-
+	NGAY x;
+	Nhap(x);
+	cout << SoNgayToiDaTrongThang(x);
 	return 0;
+}
+
+void Nhap(NGAY& x)
+{
+	cout << "Nhap ngay: ";
+	cin >> x.Ngay;
+	cout << "Nhap thang: ";
+	cin >> x.Thang;
+	cout << "Nhap nam: ";
+	cin >> x.Nam;
+}
+
+int ktNhuan(NGAY x)
+{
+	if (x.Nam % 4 == 0 && x.Nam % 100 != 0 || x.Nam % 400 == 0)
+		return 1;
+	return 0;
+}
+
+int SoNgayToiDaTrongThang(NGAY x)
+{
+	int ngaythang[12] = { 31, 28, 31, 30, 31, 30,
+	31, 31, 30, 31, 30, 31 };
+	if (ktNhuan(x))
+		ngaythang[1] = 29;
+	return ngaythang[x.Thang - 1];
 }
