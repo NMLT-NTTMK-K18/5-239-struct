16f6315c78313355529b0528e29a5ca9dfeb0527
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-24 20:53:56 +0700

    Bai145. Completed

--- 2. Bai145/Source.cpp
+++ 2. Bai145/Source.cpp
@@ -1,8 +1,73 @@
 #include <iostream>
 using namespace std;
 
+struct Diem
+{
+	float x;
+	float y;
+};
+typedef struct Diem DIEM;
+
+struct TamGiac
+{
+	DIEM A;
+	DIEM B;
+	DIEM C;
+};
+typedef struct TamGiac TAMGIAC;
+
+void Nhap(DIEM&);
+void Nhap(TAMGIAC&);
+float KhoangCach(DIEM, DIEM);
+float DangTamGiac(TAMGIAC);
+
 int main()
 {
-
+	TAMGIAC t;
+	Nhap(t);
+	cout << DangTamGiac(t);
 	return 0;
+}
+
+void Nhap(DIEM& P)
+{
+	cout << "Nhap x: ";
+	cin >> P.x;
+	cout << "Nhap y: ";
+	cin >> P.y;
+}
+
+void Nhap(TAMGIAC& t)
+{
+	cout << "Nhap A: " << endl;
+	Nhap(t.A);
+	cout << "Nhap B: " << endl;
+	Nhap(t.B);
+	cout << "Nhap C: " << endl;
+	Nhap(t.C);
+}
+
+float KhoangCach(DIEM P, DIEM Q)
+{
+	return sqrt((Q.x - P.x) * (Q.x - P.x) + (Q.y - P.y) * (Q.y - P.y));
+}
+
+float DangTamGiac(TAMGIAC t)
+{
+	float a = KhoangCach(t.B, t.C);
+	float b = KhoangCach(t.A, t.C);
+	float c = KhoangCach(t.A, t.B);
+	if (!(a + b > c && b + c > a && c + a > b))
+		return 0;
+	if (a == b && b == c)
+		return 1;
+	if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b)
+	{
+		if (a == b || b == c || a == c)
+			return 2;
+		return 3;
+	}
+	if (a == b || b == c || a == c)
+		return 4;
+	return 5;
 }
