6752d809e1cc5007e1f83289e38af164250c1b52
khiemhoangtruongUIT <137704231+truonghoangkhiem@users.noreply.github.com>
2023-10-24 23:43:15 +0700

    Fix the wrong topic 072

--- 2. Bai072/Source.cpp
+++ 2. Bai072/Source.cpp
@@ -11,19 +11,12 @@ struct HonSo
 typedef struct HonSo HONSO;
 
 void Nhap(HONSO&);
-void Xuat(HONSO);
-void RutGon(HONSO&);
-int UCLN(int, int);
 
 int main()
 {
 	HONSO M;
 	cout << "Nhap hon so M: " << endl;
 	Nhap(M);
-	RutGon(M);
-	cout << "Hon so M sau khi rut gon la:" << endl;
-	Xuat(M);
-
 	return 0;
 }
 
@@ -35,32 +28,4 @@ void Nhap(HONSO& x)
 	cin >> x.Tu;
 	cout << "Nhap mau: ";
 	cin >> x.Mau;
-}
-
-void Xuat(HONSO x)
-{
-	cout << setw(6);
-	cout << "\n Nguyen: " << x.Nguyen;
-	cout << "\n Tu: " << x.Tu;
-	cout << "\n Mau: " << x.Mau;
-}
-
-int UCLN(int a, int b)
-{
-		a = abs(a);
-		b = abs(b);
-	while (a * b != 0)
-		if (a > b)
-			a = a - b;
-		else
-			b = b - a;
-	return a + b;
-}
-void RutGon(HONSO & x)
-{
-	int kq = UCLN(x.Tu, x.Mau);
-	x.Tu = x.Tu / kq;
-	x.Mau = x.Mau / kq;
-    x.Nguyen = x.Nguyen + x.Tu / x.Mau;
-	x.Tu = x.Tu % x.Mau;
 }
