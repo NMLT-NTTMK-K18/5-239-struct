a6bfd616ec19ce92d82ed6834c4d5ea81fc35f37
NTGNguyen <23521049@gm.uit.edu.vn>
2023-10-26 09:17:51 +0700

    ✅ 2.Bai159.Done

--- 2. Bai159/Source.cpp
+++ 2. Bai159/Source.cpp
@@ -8,8 +8,23 @@ struct DaThuc
 };
 typedef struct DaThuc DATHUC;
 
+void GiamBac(DATHUC);
+void TangBac(DATHUC);
+
 int main()
 {
 
 	return 0;
+}
+
+void GiamBac(DATHUC &f)
+{
+	while (f.a[f.n] == 0)
+		f.n--;
+}
+
+void TangBac(DATHUC f)
+{
+	while (f.a[f.n] == 0)
+		f.n++;
 }
