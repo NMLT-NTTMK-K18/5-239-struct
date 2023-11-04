14b8f59ef039f5d496012b493aac38e6376624c4
khiemhoangtruongUIT <137704231+truonghoangkhiem@users.noreply.github.com>
2023-11-04 16:25:43 +0700

    212-237 completed

--- 2. Bai222/Source.cpp
+++ 2. Bai222/Source.cpp
@@ -1,8 +1,79 @@
 #include <iostream>
 using namespace std;
 
+struct diem
+{
+	float x;
+	float y;
+};
+typedef struct diem DIEM;
+
+void Nhap(DIEM&);
+void Nhap(DIEM[][100], int&, int&);
+int DemKhongTrung(DIEM a[][100], int m, int n);
+int ktTrung(DIEM, DIEM);
+int TanSuat(DIEM[][100], int, int, DIEM);
+
 int main()
 {
+	DIEM a[100][100];
+	int m, n;
+	cout << "Nhap ma tran cac diem:" << endl;
+	Nhap(a, m, n);
+
+	int kq = DemKhongTrung(a,m,n);
+	cout << "\nSo Luong Diem Khong Trung La:" << kq;
 
 	return 0;
+}
+
+void Nhap(DIEM& P)
+{
+	cout << "Nhap x: ";
+	cin >> P.x;
+	cout << "Nhap y: ";
+	cin >> P.y;
+}
+
+void Nhap(DIEM a[][100], int& m, int& n)
+{
+	cout << "Nhap so dong: ";
+	cin >> m;
+	cout << "Nhap so cot: ";
+	cin >> n;
+	int k = 1;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			cout << "\nNhap diem thu " << k++ << ": ";
+			Nhap(a[i][j]);
+		}
+}
+
+int ktTrung(DIEM P, DIEM Q)
+{
+	if (P.x == Q.x && P.y == Q.y)
+		return 1;
+	return 0;
+}
+
+int TanSuat(DIEM a[][100], int m, int n, DIEM P)
+
+{
+	int dem = 0;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			if (ktTrung(a[i][j], P))
+				dem++;
+	return dem;
+}
+
+int DemKhongTrung(DIEM a[][100], int m, int n)
+{
+	int dem = 0;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		if (TanSuat(a, m, n, a[i][j]) == 1)
+		dem++;
+	return dem;
 }
--- 2. Bai232/Source.cpp
+++ 2. Bai232/Source.cpp
@@ -1,8 +1,113 @@
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
+
+struct PhanSo
+{
+	int Tu;
+	int Mau;
+};
+typedef struct PhanSo PHANSO;
+
+void Nhap(PHANSO&);
+void Nhap(PHANSO[][100], int&, int&);
+
+void Xuat(PHANSO);
+void Xuat(PHANSO[], int);
+void Xuat(PHANSO[][100], int, int);
+
+void HoanVi(PHANSO&, PHANSO&);
+void SapTang(PHANSO[][100], int, int);
+
 
 int main()
 {
-
+	PHANSO a[100][100];
+	int m, n;
+	Nhap(a, m, n);
+	SapTang(a, m, n);
+	Xuat(a, m, n);
 	return 0;
 }
+
+void Nhap(PHANSO& x)
+{
+	cout << "\nNhap Tu:";
+	cin >> x.Tu;
+	cout << "Nhap Mau:";
+	cin >> x.Mau;
+}
+
+void Nhap(PHANSO a[][100], int& m, int& n)
+{
+	cout << "Nhap so hang: ";
+	cin >> m;
+	cout << "Nhap so cot: ";
+	cin >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Nhap a[" << i << "][" << j << "]:\n";
+			Nhap(a[i][j]);
+		}
+}
+
+
+void Xuat(PHANSO x)
+{ 
+	cout<< setw(5);
+	cout << x.Tu << "/" << x.Mau ;
+}
+
+void Xuat(PHANSO a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << "\na[" << i << "] = ";
+		Xuat(a[i]);
+	}
+}
+
+void Xuat(PHANSO a[][100], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		cout << endl;
+		for (int j = 0; j < n; j++)
+		{
+			Xuat(a[i][j]);
+
+		}
+	}
+}
+
+
+void HoanVi(PHANSO& a, PHANSO& b)
+{
+	PHANSO temp{};
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+int SoSanh(PHANSO a, PHANSO b)
+{
+	float m = (float)a.Tu / a.Mau;
+	float n = (float)b.Tu / b.Mau;
+	if (m > n)
+		return 1;
+	if (m < n)
+		return -1;
+	return 0;
+}
+
+void SapTang(PHANSO a[][100], int m, int n)
+{
+	for (int i = 0; i <= m * n - 2; i++)
+		for (int j = i + 1; j <= m * n - 1; j++)
+			if (SoSanh(a[i / n][i % n],
+				a[j / n][j % n]) == 1)
+				HoanVi(a[i / n][i % n], a[j / n][j % n]);
+}
+
